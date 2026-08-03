62da1779431a7cb10fd5a790f55c4dd66aab44ed
bhavikp-electrobit <bhavikp@electrobittech.com>
2026-08-03 17:36:12 +0530

    Add Wi-Fi STA/AP configuration, documentation, and improve RS-485 communication
    - Add Wi-Fi STA/AP configuration support
    - Add documentation for Wi-Fi STA configuration

--- Unit_Test/mocks/esp_wifi.h
+++ Unit_Test/mocks/esp_wifi.h
@@ -27,8 +27,10 @@ typedef enum {
 #define ESP_ERR_WIFI_NOT_STARTED (ESP_FAIL)
 
 typedef enum {
-    WIFI_MODE_NULL,
+    WIFI_MODE_NULL = 0,
     WIFI_MODE_STA,
+    WIFI_MODE_AP,
+    WIFI_MODE_APSTA,
 } wifi_mode_t;
 
 typedef enum {
